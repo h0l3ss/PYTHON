7764346bedc7d4fa62f979f44f188b4e74f9c349
Weverthon Gimenes <58565098+h0l3ss@users.noreply.github.com>
2023-11-06 15:17:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask (__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/')
+def hello_world():
+ return 'hello_world!'
+
+
+if __name__ == '__main__';
+
+  app.run()
