df8567ea470f40e1be3153ac989cb8dbee76a53a
Weverthon Gimenes <58565098+h0l3ss@users.noreply.github.com>
2023-11-06 14:53:54 -0300

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,11 +1,6 @@
 from flask import Flask
-app = Flask (__name__)
+app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
- return 'hello_world!'
-
-
-if __name__ == '__main__';
-
-  app.run()
+@app.route("/")
+def hello():
+    return "Hello, World!"
